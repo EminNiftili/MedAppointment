c60f36861f12ec6ef53c5a0415582a8a2516580c
Emin Hasanov <developerPazl@gmail.com>
2026-01-14 02:20:55 +0400

    Di added for data Access layer

---
+++ MedAppointment.DataAccess/AppConfig/DependencyInjectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MedAppointment.DataAccess.AppConfig
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddDataAccess(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            AddUnifOfWorks(serviceProvider);
+            AddRepositories(serviceProvider);
+            AddDbContext(serviceProvider, configuration);
+        }
+        private static void AddRepositories(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+        private static void AddUnifOfWorks(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+        private static void AddDbContext(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
