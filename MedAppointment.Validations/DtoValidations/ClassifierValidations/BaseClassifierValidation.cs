48ac1ffa08b1bbfe164bc1ce9d7aee6fe1e2537a
Emin Hasanov <developerPazl@gmail.com>
2026-01-21 03:03:58 +0400

    Classifier Name validation is not importnat

--- MedAppointment.Validations/DtoValidations/ClassifierValidations/BaseClassifierValidation.cs
+++ MedAppointment.Validations/DtoValidations/ClassifierValidations/BaseClassifierValidation.cs
@@ -10,10 +10,10 @@
                     .WithMessage("Name is required.")
                 .MaximumLength(150)
                     .WithErrorCode("ERR00041")
-                    .WithMessage("Name must not exceed 150 characters.")
-                .Must(BeValidClassifierName)
-                    .WithErrorCode("ERR00042")
-                    .WithMessage("Name contains invalid characters.");
+                    .WithMessage("Name must not exceed 150 characters.");
+            //.Must(BeValidClassifierName)
+            //    .WithErrorCode("ERR00042")
+            //    .WithMessage("Name contains invalid characters.");
 
             RuleFor(x => x.Description)
                 .NotEmpty()
@@ -21,10 +21,10 @@
                     .WithMessage("Description is required.")
                 .MaximumLength(500)
                     .WithErrorCode("ERR00044")
-                    .WithMessage("Description must not exceed 500 characters.")
-                .Must(BeValidClassifierDescription)
-                    .WithErrorCode("ERR00045")
-                    .WithMessage("Description contains invalid characters.");
+                    .WithMessage("Description must not exceed 500 characters.");
+                //.Must(BeValidClassifierDescription)
+                //    .WithErrorCode("ERR00045")
+                //    .WithMessage("Description contains invalid characters.");
 
         }
     }
